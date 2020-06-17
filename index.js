75541cda38635d7c8d3a1b439bcb97e764fd0b26
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-06-17 14:33:12 -0700

    basic server setup and running.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+const PORT = 5000
+
+server.get('/', (req, res) => {
+  res.json({ message: "randooommm"})
+})
+
+server.listen(PORT, () => {
+  console.log(`\n*** Server running on port ${PORT} ***\n`)
+})
+
