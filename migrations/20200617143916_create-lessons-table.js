ffe086a7508a6870676d85a9455abe4f3b498c24
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-06-17 14:49:44 -0700

    initialized db using knex.

---
+++ migrations/20200617143916_create-lessons-table.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('lessons', tbl => {
+    tbl.increments() // id field
+    tbl.text('name', 128)
+      .notNullable()
+    tbl.timestamps(true, true)
+  })
+  .createTable('messages', tbl => {
+    tbl.increments()
+    tbl.string('sender')
+      .notNullable()
+      .index()
+    tbl.text('text')
+      .notNullable()
+    tbl.timestamps(true, true)
+    // foreign key infor to 'lessons' table
+    tbl.integer('lesson_id')
+      .unsigned()
+      .references('id')
+      .inTable('lessons')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('messages').dropTableIfExists('lessons')
+};
