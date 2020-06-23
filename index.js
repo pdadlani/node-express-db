a6110a9a1fa714088b9acb17b25e9a2c1b186359
Priyanka Dadlani <dadlani.priyanka@gmail.com>
2020-06-22 17:16:11 -0700

    wrote queries to db using knex.js.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const express = require('express')
+const express = require('express');
+const Lessons = require('./models/dbHelpers');
 
-const server = express()
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
 
-const PORT = 5000
+const PORT = 5001;
 
 server.get('/', (req, res) => {
-  res.json({ message: "randooommm"})
+  res.json({ message: "randooommm"});
+});
+
+server.post('/api/lessons', (req, res) => {
+  Lessons.add(req.body)
+    .then(lesson => {
+      res.status(200).json(lesson);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Cannot add lesson"});
+    });
+});
+
+server.get('/api/lessons', (req, res) => {
+  Lessons.find()
+    .then(lessons => {
+      res.status(200).json(lessons);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "unable to retrieve lessons."})
+    })
 })
 
 server.listen(PORT, () => {
-  console.log(`\n*** Server running on port ${PORT} ***\n`)
-})
+  console.log(`\n*** Server running on port ${PORT} ***\n`);
+});
 
